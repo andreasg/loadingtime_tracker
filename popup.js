1e456a916e0857832ad95d1e8c0933b30d31d7fd
Andreas Granström <andreas.granstrom@gmail.com>
2015-01-26 21:43:05 +0000

    tweaking

--- popup.js
+++ popup.js
@@ -1,14 +1,9 @@
-function clear_timer() {
-    chrome.storage.local.clear(null);
-}
-
 function add_message() {
     chrome.storage.local.get('load_times', function(value) {
-	console.log(value.load_times);
-	
 	var total = 0;
-	for (var i=0; i<value.load_times.length; i++) {
-	    total += value.load_times[i];
+
+	if ('load_times' in value) {
+	    total = value.load_times;
 	}
 
 	var div = document.getElementById('wastemetermessage');
--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 var t0 = new Date().getTime();
 $(document).ready(function(){
     var t1 = new Date().getTime();
-
     chrome.storage.local.get('load_times', function(value) {
-	if (value.load_times) {
-	    value.load_times.push(t1 - t0);
+	if ('load_times' in value) {
+	    value.load_times += (t1 - t0);
 	} else {
-	    value.load_times = [t1 - t0];
+	    value.load_times = 0;
 	}
 	chrome.storage.local.set(value, function () { });
     });
