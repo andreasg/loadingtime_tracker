71ebd5bcc84a84736819a296881925a005af30da
Andreas Granström <andreas.granstrom@gmail.com>
2015-01-27 18:57:13 +0100

    log time on daily basis

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
-function add_message() {
-    chrome.storage.local.get('load_times', function(value) {
-	var total = 0;
+"use strict";
 
-	if ('load_times' in value) {
-	    total = value.load_times;
+var add_message = function() {
+    chrome.storage.local.get(null, function(value) {
+	var total_time_waited = 0;
+	for (var datestr in value) {
+	    total_time_waited += value[datestr];
 	}
-
 	var div = document.getElementById('wastemetermessage');
 	var msg = "<p>You have spent ";
-	msg += total;
+	msg += total_time_waited;
 	msg += " milliseconds of your life waiting for pages to load!</p>";
 	div.innerHTML += msg;
     });
-}
-add_message();
+}();
+
--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 "use strict";
 
-var t0 = new Date().getTime();
-$(document).ready(function(){
-    var t1 = new Date().getTime();
-    chrome.storage.local.get('load_times', function(value) {
-	if ('load_times' in value) {
-	    value.load_times += (t1 - t0);
-	} else {
-	    value.load_times = 0;
-	}
-	chrome.storage.local.set(value, function () { });
+var log_load_time = function() {
+    var t0 = new Date().getTime();
+    $(document).ready(function(){
+	var t1 = new Date().getTime();
+	var td = t1 - t0;
+	chrome.storage.local.get(null, function(value) {
+	    var current_date = (new Date()).toISOString().substr(0, 10);
+	    if (current_date in value) {
+		value[current_date] += td;
+	    } else {
+		value[current_date] = td;
+	    }
+	    chrome.storage.local.set(value, function () { });
+	});
     });
-});
+}();
